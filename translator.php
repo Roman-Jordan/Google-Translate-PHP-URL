6a48fd4cf54285cdbf98f85fa87ad47f84e693b5
AlphaRomulus <rjordan1352@gmail.com>
2019-04-04 22:51:00 -0700

    Converted to a more Object oriented Approch, Still need make some other cool stuff happen thought.

--- translator.php
+++ translator.php
@@ -2,45 +2,64 @@
 <?php 
 
 class Translate{
-  function supportedLanguages($languages){
-    
-    $class = isset($languages['class']) ? 'class="'.$languages['class'].'"': false;
-    $id = isset($languages['id']) ? 'id="'.$languages['id'].'"': false;
-    $dir = isset($languages['dir']) ? $languages['dir'] : 'languages';
-    $tag = isset($languages['tag']) ? $languages['tag'] : 'button';
-    $href = isset($languages['href']) ? $languages['href'] : false;
-    
-    $path = $dir.'/supported-languages.json';
-    
-    if(file_exists($path)){
-        $pull = file_get_contents($path);
-        $decoded = json_decode($pull);
-        $html = "";
-        foreach($decoded->data->languages as $language){
-            $html .= '<'.$tag.' '.$class.' '.$id.' value="'.$language->language.'">'.$language->name.'</'.$tag.'>';
-        }
-        return $html;
-    }else{
-        $this->toNative($languages);
-    }  
-    return 'complete';  
+  
+  function __construct($languages){
+    foreach($languages as $key => $value){
+      $this->$key = $value;
+    }
+    $this->path = $this->directory. '/supported-languages.json';
   }
 
-  function toNative($languages){
-    $pull = file_get_contents("https://translation.googleapis.com/language/translate/v2/languages?target=en&key=".$languages['apiKey']);
+  function supportedLanguages(){
+    //Defaults
+    $class = isset($this->class) ? 'class="'.$this->class.'"': false;
+    $id = isset($this->id) ? 'id="'.$this->id.'"': false;
+    $dir = isset($this->dir) ? $this->dir : 'languages';
+    $tag = isset($this->tag) ? $this->tag : 'button';
+    $href = isset($this->href) ? $this->href : false; 
+    
+    if(file_exists($this->path)){
+        $this->escapeHTML();
+    }else{
+        $this->toNative();
+    }  
+  }
+
+  function toNative(){
+    $pull = file_get_contents("https://translation.googleapis.com/language/translate/v2/languages?target=en&key=".$this->apiKey);
     $languageList = json_decode($pull);
+    
     foreach($languageList->data->languages as $language){
-      $languageToNative = file_get_contents('https://translation.googleapis.com/language/translate/v2/?target='.$language->language.'&q='.$language->name.'&key='.$languages['apiKey']);
-      $nativeLang = json_decode($languageToNative);
-      foreach($nativeLang->data->translations as $lang){
-        $language->name = $lang->translatedText;
-        $html .= '<'.$tag.' '.$class.' '.$id.' value="'.$language->language.'">'.$language->name.'</'.$tag.'>';
+      
+      $languageToNative = @file_get_contents('https://translation.googleapis.com/language/translate/v2/?target='.$language->language.'&q='.$language->name.'&key='.$this->apiKey);
+      if($languageToNative === false){
+        $language->name = NULL;
+      }else{
+        $nativeLang = json_decode($languageToNative);
+        foreach($nativeLang->data->translations as $lang){
+          $language->name = $lang->translatedText;
+        }
       }
     }
-    $file = fopen("languages/supported-languages.json", "w") or die("Unable to open file!");
+    
+    $file = fopen($this->path, "w") or die("Unable to open file!");
     fwrite($file, json_encode($languageList));
     fclose($file);
-    $this->supportedLanguages($languages);
+    
+    $this->escapeHTML();
+  }
+
+  function escapeHTML(){
+    
+    $pull = file_get_contents(__DIR__.'/'.$this->path);
+    $decoded = json_decode($pull);
+    $html = "";
+    foreach($decoded->data->languages as $language){
+      if($language->name !== NULL){
+        $html .= '<'.$this->tag.' '.$this->class.' '.$this->id.' value="'.$language->language.'">'.$language->name.'</'.$this->tag.'>';
+      }
+    }
+    ECHO $html;
   }
 }
 
@@ -55,7 +74,7 @@ $languages = array(
     'id'        => 'Hello'
 );
 
-$translate = new Translate();
+$translate = new Translate($languages);
 ?>
  
 
@@ -63,29 +82,13 @@ $translate = new Translate();
 <html lang="en" style="height:100%;" wp-site wp-site-is-master-page>
     <head>
         <meta charset="utf-8">
-        <title>FarmHealth Technologies</title>
+        <title>Translation Catching</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="keywords" content="pinegrow, blocks, bootstrap" />
-        <meta name="description" content="My new website" />
-        <link rel="shortcut icon" href="ico/favicon.png">
-        <!-- Core CSS -->
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/font-awesome.min.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet">
-        <!-- Style Library -->
-        <link href="css/style-library-1.css" rel="stylesheet">
-        <link href="css/plugins.css" rel="stylesheet">
-        <link href="css/blocks.css" rel="stylesheet">
-        <link href="css/custom.css" rel="stylesheet">
-        <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
-        <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
+        
     <![endif]-->
     </head>
     <body data-spy="scroll" data-target="nav">
-        <header id="header-3" class="soft-scroll header-3" wp-cz-section="blocks_header_3" wp-cz-section-title="Header 3">
+        <section id="header-3" class="soft-scroll header-3" wp-cz-section="blocks_header_3" wp-cz-section-title="Header 3">
             <!-- /.nav -->
                         <!-- added translator below -->
             <div class="col-md-12">
@@ -101,42 +104,20 @@ $translate = new Translate();
                 <p class="tx">I am text to be translated in independent tags 6.</p>
                 <p class="tx">I am text to be translated in independent tags 7.</p>
                 <p class="tx">I am text to be translated in independent tags 8.</p>
-            </div>              
-        </header>
-     
-        <!--Modal-->
-         <!-- Modal -->
-  <div class="modal fade" id="translate-modal" role="dialog">
-        <div class="modal-dialog">
-        
-          <!-- Modal content-->
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h4 class="modal-title">Please Select a language</h4>
-            </div>
+            </div>  
+
             <div class="modal-body" id="languages">
               <?php echo $translate->supportedLanguages($languages);?>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-      
-    </div>
 
-
-
-        <!--/Modal-->
-        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="js/plugins.js"></script>
-        <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
-        <script type="text/javascript" src="js/bskit-scripts.js"></script>
-        
+          </section>
+          <script>
+            let stringsToObj = {};
+            document.querySelectorAll(".tx").forEach((string, x) => {
+                stringsToObj[x] = !stringsToObj[x] ? string.innerHTML:stringsToObj[x] +1;
+            });
+            console.log(stringsToObj);
+          </script>
     </body>
 </html>
 
